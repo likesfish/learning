664439321e1c83fc2ced0c5c169160f93c00407c
Lauren Rodriguez <lauren.rodriguez189@gmail.com>
2017-07-28 18:37:11 -0400

    adds group size option, thanks derek!

--- homework5.py
+++ homework5.py
@@ -7,9 +7,14 @@ while names.lower() != "done":
     names = raw_input('What is your name? ')
     name.append(names)
 name = name[0:-1]
-print name
+
+number = raw_input('What size groups would you like? ')
+group = int(number)
 
 random.shuffle(name)
 uhoh = len(name)
-for names in range(0,uhoh,2):
-    print name[names],name[names+1]
+print '-----'
+for names in range(0,uhoh,group):
+    for i in range(names, group + names):
+        print name[i]
+    print '-----'
