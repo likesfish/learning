fe2efa450d2cdbd20e3c08da95a3f89266ea5df8
Lauren Rodriguez <lauren.rodriguez189@gmail.com>
2017-08-06 20:43:58 -0400

    Fixes #2

--- homework5.py
+++ homework5.py
@@ -21,6 +21,8 @@ def make_groups(names, group_size):
         group = []
         for i in range(boundary, boundary + group_size, 1):
             #i am appending the name [i] in the list of names
+            if i >= len(names):
+                break
             group.append(names[i])
         all_groups.append(group)
     return all_groups
