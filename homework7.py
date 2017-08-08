b455211efd630fcc974a30756ac242e7327b0467
Lauren Rodriguez <lauren.rodriguez189@gmail.com>
2017-08-08 19:21:47 -0400

    i'm making a website!

---
+++ homework7.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    sentence = "Once I needed to be in a group and didn't have any friends"
+    return sentence
+
+@app.route("/fish")
+def fish():
+    return "fish live underwater"
