f442d1b55c79b1091fd02d297dd216e0a44f1dd6
Lauren Rodriguez <lauren.rodriguez189@gmail.com>
2017-09-04 14:58:14 -0400

    I'm learning a class

--- shop_owner.py
+++ shop_owner.py
@@ -1,3 +1,5 @@
+from product import Product
+
 inventory  = []
 dictionary = {}
 
@@ -7,25 +9,23 @@ while True:
         break
     number_item = raw_input('How many do you have? ')
     number_item = int(number_item)
-    dictionary[item] = number_item
+    dictionary[item] = Product(item, quantity=number_item)
 print dictionary
 
 while True:
     first_question = raw_input('What would you like to buy? ')
     if first_question in dictionary:
-        print 'we have: ', dictionary[first_question]
+        product = dictionary[first_question]
+        print 'we have: ', product.quantity , product.name + 's'
     else:
         print "I'm sorry we don't have that"
         continue
     second_question = raw_input('How many would you like? ')
     second_question = int(second_question)
 
-    #how many we will have = have
-    have = dictionary[first_question] - second_question
 
-    if have >= 0:
-        print 'after you make this purchase we will have: ', have
-        dictionary[first_question] = have
-    else:
+    try:
+        product.sell(second_question)
+    except Exception as e:
         print 'We don\'t have that many, or don\'t have any in stock'
-        print 'but we have', dictionary[first_question]
+        print 'but we have', product.quantity
